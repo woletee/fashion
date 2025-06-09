d9eab72b73baf0b828e671008a5d1c8fe5dc786c
Liyew <118590747+woletee@users.noreply.github.com>
2025-06-09 18:54:53 +0900

    Update Wardrobe.js

--- src/components/Wardrobe.js
+++ src/components/Wardrobe.js
@@ -4,7 +4,7 @@ function Wardrobe() {
   const [items, setItems] = useState([]);
 
   useEffect(() => {
-    fetch('http://54.174.88.108:8000/wardrobe/items')
+    fetch('https://wardrobestudio.net/wardrobe/items')
       .then(res => res.json())
       .then(data => setItems(data))
       .catch(err => console.error('Error loading wardrobe:', err));
@@ -22,7 +22,7 @@ function Wardrobe() {
       <div className="wardrobe-grid">
         {items.map((item) => (
           <div key={item.id} className="clothing-item">
-            <img src={`http://54.174.88.108:8000${item.image_url}`} alt={item.name} width="120" />
+            <img src={`https://wardrobestudio.net/${item.image_url}`} alt={item.name} width="120" />
             <p>{item.name} ({item.category})</p>
           </div>
         ))}
