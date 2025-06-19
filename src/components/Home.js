ebd64fd4de70adea23022968e694b59606034515
Liyew <118590747+woletee@users.noreply.github.com>
2025-06-20 08:14:50 +0900

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,9 +4,9 @@ function Home() {
   return (
     <div className="screen">
       <h2>Today's Weather</h2>
-      <p>55°F / 45°F 🌤️</p>
+      <p>27°c / 28°c 🌤️</p>
       <div className="upload-prompt">
-        <p>Add 30 items and get outfits for tomorrow!</p>
+        <p>Add 10 items and get outfits for tomorrow!</p>
         <button>+ Add Item</button>
       </div>
     </div>
